d903eebe669dfc93491dea2b96aed13053e8eedf
Avi Bryant <avi@stripe.com>
2018-04-12 13:22:56 -0700

    add tracing and use it in FitHLL

--- src/main/scala/rainier/models/FitHLL.scala
+++ src/main/scala/rainier/models/FitHLL.scala
@@ -14,7 +14,7 @@ object FitHLL {
       hll.logDensity(lambda, sketch)
     }
 
-  def compare(scale: Int) = {
+  def compare(scale: Int): RandomVariable[Real] = {
     println("Generating a set with max size " + scale)
     val data = 1.to(scale).map { i =>
       rand.nextInt
@@ -27,17 +27,34 @@ object FitHLL {
     println("Confidence interval: " + lower.toInt + ", " + upper.toInt)
 
     println("Inferring size")
-    val samples = model(sketch).sample()
+    val m = model(sketch)
+    val t1 = System.currentTimeMillis
+    val samples =
+      m.sample(Hamiltonian(10000, 1000, 100, 100, SampleHMC, 1, 0.01))
+    val t2 = System.currentTimeMillis
     val mean = samples.sum / samples.size
     println("Inferred size: " + mean.toInt)
     val sorted = samples.sorted
     val lower2 = sorted((samples.size * 0.05).toInt)
     val upper2 = sorted((samples.size * 0.95).toInt)
     println("Credible interval: " + lower2.toInt + ", " + upper2.toInt)
+    println("ms: " + (t2 - t1))
     println("")
+    m
   }
 
   def main(args: Array[String]) {
     compare(1000)
+  }
+}
+
+object TraceHLL {
+  def main(args: Array[String]) {
+    val m = FitHLL.compare(1000)
+    val d = m.density
+    val v = Real.variables(d).toList.head
+    val g = Gradient.derive(List(v), d).head
+    val c = Compiler(List(d, g))
+    c.trace
   }
 }
