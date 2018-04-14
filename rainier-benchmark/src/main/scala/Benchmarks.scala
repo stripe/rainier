ded7812c46654041b5914bea81f97893a40057c4
Avi Bryant <avi@stripe.com>
2018-04-14 14:52:50 -0700

    move to a multiproject sbt setup

---
+++ rainier-benchmark/src/main/scala/Benchmarks.scala
@@ -0,0 +1,10 @@
+package rainier.bench
+
+import org.openjdk.jmh.annotations.Benchmark
+
+class Benchmarks {
+  @Benchmark
+  def wellHelloThere(): Unit = {
+    // this method was intentionally left blank.
+  }
+}
---
+++ rainier-example/build.sbt
@@ -0,0 +1,1 @@
+name := "rainier-example"
