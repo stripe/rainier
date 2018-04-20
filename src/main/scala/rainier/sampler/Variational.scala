dedef777a591b52019593f74df83f0c37070de4b
Grzegorz Kossakowski <gkk@stripe.com>
2018-04-19 17:32:51 -0700

    a seed for variational inference

--- src/main/scala/rainier/sampler/Variational.scala
+++ src/main/scala/rainier/sampler/Variational.scala
@@ -1,6 +1,6 @@
 package rainier.sampler
 
-import rainier.compute.Real
+import rainier.compute.{Real, Variable}
 
 case class Variational(tolerance: Double, maxIterations: Int) extends Sampler {
   override def description: (String, Map[String, Double]) = ("Variational",
@@ -12,9 +12,14 @@ case class Variational(tolerance: Double, maxIterations: Int) extends Sampler {
   override def sample(density: Real)(implicit rng: RNG): Iterator[Sample] = {
     //VariationalOptimizer(tolerance, maxIterations)
     val vars = Real.variables(density)
-    val params = vars.map { v =>
+    val lambdas = vars.map { v =>
             v -> rng.standardNormal
           }.toMap
-    //def q(lambda: )
+//    def q(lamb)
+    def q(lambdas: Map[Variable, Double])
+    def elbo(lambdas: Map[Variable, Double]) = {
+      // expectation(density) - expectation(density_variational(lambdas))
+      // sum(
+    }
   }
 }
