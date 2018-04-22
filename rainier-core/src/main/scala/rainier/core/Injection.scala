537c33eda10041bbd155cb62a8f9ce6227fc6b0f
Avi Bryant <avi@stripe.com>
2018-04-21 17:57:14 -0700

    oh right this is where we need jacobians?

--- rainier-core/src/main/scala/rainier/core/Injection.scala
+++ rainier-core/src/main/scala/rainier/core/Injection.scala
@@ -7,6 +7,13 @@ trait Injection {
   def backwards(y: Real): Real
   def isDefinedAt(y: Double): Boolean
 
+  /*
+    See https://en.wikipedia.org/wiki/Probability_density_function#Dependent_variables_and_change_of_variables
+    This function should be log(d/dy backwards(y)), where y = forwards(x).
+    TODO: better comment here
+   */
+  def logJacobian(x: Real): Real
+
   def transform(dist: Continuous): Continuous = new Continuous {
     override def logDensity(t: Double) =
       if (isDefinedAt(t))
@@ -14,8 +21,10 @@ trait Injection {
       else
         Real.zero
 
-    def realLogDensity(real: Real) =
-      dist.realLogDensity(backwards(real))
+    def realLogDensity(real: Real) = {
+      val t = backwards(real)
+      dist.realLogDensity(t) + logJacobian(t)
+    }
 
     val generator = Generator.from { (r, n) =>
       n.toDouble(forwards(dist.generator.get(r, n)))
@@ -29,16 +38,19 @@ case class Scale(a: Real) extends Injection {
   def forwards(x: Real) = x * a
   def backwards(y: Real) = y / a
   def isDefinedAt(y: Double) = true
+  def logJacobian(x: Real) = Real.zero
 }
 
 case class Translate(b: Real) extends Injection {
   def forwards(x: Real) = x + b
   def backwards(y: Real) = y - b
   def isDefinedAt(y: Double) = true
+  def logJacobian(x: Real) = Real.zero
 }
 
 object Exp extends Injection {
   def forwards(x: Real) = x.exp
   def backwards(y: Real) = y.log
   def isDefinedAt(y: Double) = y >= 0
+  def logJacobian(x: Real) = x
 }
