d8e9d6eda5ea989220ebf26a8f958b4a5f63447e
Jonathan Glidden <jglidden@stripe.com>
2018-04-22 18:55:45 -0700

    closer to making types match...

--- src/main/scala/rainier/sampler/Variational.scala
+++ src/main/scala/rainier/sampler/Variational.scala
@@ -19,13 +19,13 @@ case class Variational(tolerance: Double, maxIterations: Int) extends Sampler {
 
     // use a set of independent normals as the guide
     val K = modelVariables.length
-    val iterations = 100
-    val samples = 100
+    val niterations = 100
+    val nsamples = 100
     val stepsize = .1
 
 
     def sampleFromGuide(): List[Double] = {
-      1.to(K).map { i =>
+      1.to(K).map { _ =>
         rng.standardNormal
       }
     }
@@ -37,23 +37,34 @@ case class Variational(tolerance: Double, maxIterations: Int) extends Sampler {
     }
 
     val guideDensity = eps.foldLeft(Real(0.)) {case (d, e) => d + e.density}
-    val transformedDensity = density // need to change this to density wrt fixed eps
+    // need to change this to density wrt fixed eps.
+    // can I just modify the gradients?
+    // are gradients/variables ordered in any way?
+    val transformedDensity = density
     val surrogateLoss = transformedDensity + guideDensity
     val variables = Real.variables(surrogateLoss).toList
     val guideVariables = Real.variables(guideDensity).toList
     val gradients = Gradient.derive(variables, surrogateLoss)
     val cf = Compiler(surrogateLoss :: gradients)
 
-    val initialValues = guideVariables.map(v => v -> sampleFromGuide())
-      .toMap
+    val initialValues = guideVariables.flatMap(_ => List(0., 1.))
 
-    val finalValues = 1.to(iterations).foldLeft(initialValues) {
+    val finalValues = 1.to(niterations).foldLeft(initialValues) {
       case (values, _) =>
-        // for i in 1 to nsamples
-        // set modelVariables to sample from guide with values
-        // set variables to modelVariables ++ values
-        // grads[i] = cf(variables)
-        // update = values + 1/nsamples * sum(grads)
+        val grads = 1.to(nsamples).foldLeft(values) {
+          case(grad, _) => {
+            val samples = sampleFromGuide()
+            val inputs = variables.zip(samples ++ values).toMap
+            val outputs = cf(inputs)
+            // how to get just the guide gradients?
+            values.zip(grad).map {
+              case (_, g) => grad + (1. / nsamples) * outputs(g)
+            }
+          }
+        }
+        values.zip(grads).map {
+          case(v, g) => v + stepsize * g
+        }
 
     }
 
