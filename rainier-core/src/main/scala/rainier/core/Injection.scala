50b2ec4e2fbec2c74cf2c030fc9aa8d4cd8cd343
Avi Bryant <avi@stripe.com>
2018-04-25 10:33:32 -0700

    reset exp jacobian

--- rainier-core/src/main/scala/rainier/core/Injection.scala
+++ rainier-core/src/main/scala/rainier/core/Injection.scala
@@ -52,5 +52,5 @@ object Exp extends Injection {
   def forwards(x: Real) = x.exp
   def backwards(y: Real) = y.log
   def isDefinedAt(y: Double) = y >= 0
-  def logJacobian(x: Real) = Real.zero
+  def logJacobian(x: Real) = x
 }
