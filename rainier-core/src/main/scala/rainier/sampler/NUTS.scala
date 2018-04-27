76ee29e6f6dbbe6d0598d7bad923978d4ec5212e
Avi Bryant <avi@stripe.com>
2018-04-26 17:49:09 -0700

    fix

--- rainier-core/src/main/scala/rainier/sampler/NUTS.scala
+++ rainier-core/src/main/scala/rainier/sampler/NUTS.scala
@@ -2,8 +2,16 @@ package rainier.sampler
 
 import rainier.compute._
 
-case class NUTS(maxDepth: Int = 6, initialStepSize: Double = 1.0)
-    extends Sampler {
+case class NUTS(stepSize: Double = 1.0, maxDepth: Int) extends Sampler {
   def sample(density: Real, warmupIterations: Int)(
-      implicit rng: RNG): Stream[Sample] = ???
+      implicit rng: RNG): Stream[Sample] =
+    toStream(density, HamiltonianChain(density.variables, density))
+
+  private def toStream(density: Real,
+                       chain: HamiltonianChain): Stream[Sample] = {
+    val eval = new Evaluator(density.variables.zip(chain.hParams.qs).toMap)
+    Sample(chain.accepted, eval) #:: toStream(
+      density,
+      chain.nextNUTS(stepSize, maxDepth))
+  }
 }
