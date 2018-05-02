4d7f155639918577b548363ad1aff66016b20980
Avi Bryant <avi@stripe.com>
2018-05-01 22:43:30 -0700

    wip

--- rainier-core/src/main/scala/rainier/compute/LineOptimizer.scala
+++ rainier-core/src/main/scala/rainier/compute/LineOptimizer.scala
@@ -3,7 +3,19 @@ package rainier.compute
 private object LineOptimizer {
 
   def apply(line: Line): Real = ???
+
 /*
+  def +(other: Real) = other match {
+    case Constant(v)   => Line(Map(this -> 1.0), v)
+    case nc: NonConstant =>
+      Line(Map(this -> 1.0, nc -> 1.0), 0.0)
+  }
+
+  def *(other: Real): Real = other match {
+    case Constant(v)     => Line(Map(this -> v), 0.0)
+  }
+
+
 	+
 -    case Constant(v)     => new Line(ax, b + v)	
 -    case l: Line         => new Line(merge(ax, l.ax), l.b + b)	
@@ -13,12 +25,12 @@ private object LineOptimizer {
 -    case Constant(v) => new Line(ax.map { case (r, d) => r -> d * v }, b * v)
 */
 
-  def factor(line: Line): Option[(NonConstant, Double)] = ???
-  
   def log(line: Line): Option[Real] =
-    factor(line).map{case (y,k) =>
-      y.log + Math.log(k)
-    }
+    factor(line)
+      .filter(_._2 >= 0)
+      .map{case (y,k) =>
+        y.log + Math.log(k)
+      }
 
   def pow(line: Line, exponent: Real): Option[Real] =
     exponent match {
@@ -28,6 +40,9 @@ private object LineOptimizer {
         }
       case _ => None
     }
+
+  def sum(left: Line, right: Line): Real = 
+    Line(merge(left.ax, right.ax), left.b + right.b)
 
   def multiply(left: Line, right: Line): Option[Real] =
     if(left.ax.size == 1 && right.ax.size == 1)
@@ -41,6 +56,64 @@ private object LineOptimizer {
     else
       None
 
+  //if the result is Some((y,k)), then y*k==line, k != 1
+  def factor(line: Line): Option[(NonConstant, Double)] =
+    if (line.ax.size == 1 && line.b == 0) {
+      val a = line.ax.head._2
+      val x = line.ax.head._1
+      if(a == 1.0)
+        None
+      else
+        Some((x, a))
+    }	else {	
+      val mostSimplifyingFactor =	
+        line.ax.values	
+          .groupBy(_.abs)	
+          .map { case (d, l) => d -> l.size }	
+          .toList	
+          .sortBy(_._2)	
+          .last	
+          ._1	
+
+      if(mostSimplifyingFactor == 1.0)
+        None
+      else {
+        val newAx = line.ax.map {	
+          case (r, d) => r -> d / mostSimplifyingFactor	
+        }	
+	
+        val newB = b / mostSimplifyingFactor	
+	
+        Some((Line(newAx, newB), mostSimplifyingFactor))	
+      }
+    }	
+  }	
+
   private def multiply1D(a: Double, x: NonConstant, b: Double, c: Double, y: NonConstant, d: Double): Option[Real] = ???
 
+
+  private def merge(	
+      left: Map[NonConstant, Double],	
+      right: Map[NonConstant, Double]): Map[NonConstant, Double] = {	
+    val (big, small) =	
+      if (left.size > right.size)	
+        (left, right)	
+      else	
+        (right, left)	
+	
+    small.foldLeft(big) {	
+      case (acc, (k, v)) =>	
+        val newV = big	
+          .get(k)	
+          .map { bigV =>	
+            bigV + v	
+          }	
+          .getOrElse(v)	
+
+        if (newV == 0.0)	
+          acc - k	
+        else	
+          acc + (k -> newV)	
+    }	
+  }
 }
