79ec37fa35b0bd7a2895d9816f142f4d1d1a6699
Mio Alter <mio@stripe.com>
2018-05-02 09:54:05 -0700

    starting

--- rainier-core/src/main/scala/rainier/sampler/HamiltonianIntegrator.scala
+++ rainier-core/src/main/scala/rainier/sampler/HamiltonianIntegrator.scala
@@ -38,6 +38,19 @@ private case class LeapFrogIntegrator(
     )
   }
 
+  private def halfStepPsReal(stepSize: Variable, ps: Seq[Variable], gradPotential: Seq[Real]): Seq[Real] = {
+    ps
+    .zip(gradPotential)
+    .map{ (p, grad) => p - (stepSize / 2) * grad }
+  }
+
+  private def fullStepQsReal(stepSize: Variable, qs: Seq[Variable], ps: Seq[Variable]): Seq[Variable] = {
+    qs
+    .zip(ps)
+    .map{ case (q, p) => p + (stepSize * p) }
+  }
+
+  def stepReal(qs: Seq[Variable], gradPotential: Seq[Real])
   def step(hParams: HParams, stepSize: Double): HParams = {
     val halfNewPs = halfStepPs(hParams, stepSize)
     val newQs = fullStepQs(halfNewPs, stepSize)
