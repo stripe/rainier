f2ed2829c07fff25ada054efb1acc04ee672a4cf
Mio Alter <mio@stripe.com>
2018-05-02 17:47:52 -0700

    Revert "make one CF"
    This reverts commit eaf067503cf93e49e9abbf4dcc98d8cf4eb9b4d7.
    go back to composition of CFs for now

--- rainier-core/src/main/scala/rainier/sampler/HamiltonianIntegrator.scala
+++ rainier-core/src/main/scala/rainier/sampler/HamiltonianIntegrator.scala
@@ -45,83 +45,55 @@ private case class LeapFrogIntegrator(
   }
 }
 
-private case class LeapFrogVars[+A](potential: A,
-                                    qs: Seq[A],
-                                    ps: Seq[A],
-                                    grad: Seq[A]) {
-  val toSeq: Seq[A] = potential +: (qs ++ ps ++ grad)
-}
-
-private object LeapFrogVars {
-  def apply[A](s: Seq[A]): LeapFrogVars[A] = {
-    require((s.size - 1) / 3 == 0, "there must be 3n + 1 vars!")
-    val nVars = (s.size - 1 / 3).toInt
-    LeapFrogVars(
-      s.head,
-      s.slice(1, nVars + 1),
-      s.slice(nVars + 1, 2 * nVars + 1),
-      s.slice(2 * nVars + 1, 3 * nVars + 1)
-    )
-  }
-}
-
-private case class RealLeapFrogIntegrator(variables: Seq[Variable],
-                                          density: Real)
+private case class RealLeapFrogIntegrator(
+    variables: Seq[Variable],
+    cf: Array[Double] => (Double, Array[Double]))
     extends HamiltonianIntegrator {
 
   val nVars = variables.size
-  val potential = new Variable
   val momenta = (1 to nVars).map { _ =>
     new Variable
   }
-  val grad = (1 to nVars).map { _ =>
-    new Variable
-  }
-  val inputs = LeapFrogVars(potential, variables, momenta, grad)
+  val inputs = variables ++ momenta
 
-  private def halfStepPs(stepSize: Double)(inputs: LeapFrogVars[Real]) = {
-    val newPs = inputs.ps
-      .zip(inputs.grad)
+  private def halfStepPs(inputs: Seq[Variable],
+                         realGrad: Seq[Real],
+                         stepSize: Double) = {
+    val (qs, ps) = (inputs.take(nVars), inputs.drop(nVars))
+    val newPs = ps
+      .zip(realGrad)
       .map { case (p, grad) => p - (stepSize / 2) * grad }
 
-    inputs.copy(ps = newPs)
+    Compiler.default.compile(inputs, qs ++ newPs)
   }
 
-  private def fullStepQs(stepSize: Double)(inputs: LeapFrogVars[Real]) = {
-    val newQs = inputs.qs
-      .zip(inputs.ps)
+  private def fullStepQs(inputs: Seq[Variable], stepSize: Double) = {
+    val (qs, ps) = (inputs.take(nVars), inputs.drop(nVars))
+    val newQs = qs
+      .zip(ps)
       .map { case (q, p) => q + (stepSize * p) }
-    inputs.copy(qs = newQs)
+    Compiler.default.compile(inputs, newQs ++ ps)
   }
-
-  private def updateGrad(inputs: LeapFrogVars[Real]): LeapFrogVars[Real] =
-    ??? //{
-//    val newGrad = Gradient.derive(inputs.qs, density * -1)
-//    inputs.copy(grad = newGrad)
-//  }
-
-  private def cfOutput(stepSize: Double): Seq[Real] = {
-    (halfStepPs(stepSize) _
-      andThen fullStepQs(stepSize) _
-      andThen updateGrad _
-      andThen halfStepPs(stepSize) _)(inputs).toSeq
-  }
-
-  private def leapFrogCF(stepSize: Double) =
-    Compiler.default.compile(inputs.toSeq, cfOutput(stepSize))
 
   def step(hParams: HParams, stepSize: Double): HParams = {
-    val array = LeapFrogVars(potential = hParams.potential,
-                             qs = hParams.qs,
-                             ps = hParams.ps,
-                             grad = hParams.gradPotential).toSeq.toArray
-    val outArray = leapFrogCF(stepSize)(array)
-    val outVars = LeapFrogVars(outArray.toSeq)
+    val inputArray = hParams.qs ++ hParams.ps
+    val initialRealGrad = hParams.gradPotential.map { Real(_) }
+    val cf0 = halfStepPs(inputs, initialRealGrad, stepSize)
+    val cf1 = fullStepQs(inputs, stepSize)
+
+    val newQsHalfNewPs = (cf0 andThen cf1)(inputArray)
+    val newQs = newQsHalfNewPs.take(nVars)
+    val (potential, gradPotential) = cf(newQs)
+
+    val updatedRealGrad = gradPotential.map { Real(_) }
+    val cf2 = halfStepPs(inputs, updatedRealGrad, stepSize)
+
+    val newQsAndPs = cf2(newQsHalfNewPs)
     hParams.copy(
-      potential = outVars.potential,
-      qs = outVars.qs.toArray,
-      ps = outVars.ps.toArray,
-      gradPotential = outVars.grad.toArray
+      potential = potential,
+      gradPotential = gradPotential,
+      qs = newQsAndPs.take(nVars),
+      ps = newQsAndPs.drop(nVars),
     )
   }
 }
