5650bf9270309af9a226d97f0060dd71d931fda8
Avi Bryant <avi@stripe.com>
2018-05-25 11:01:04 -0700

    flesh out diagnostics with ess

--- rainier-core/src/main/scala/com/stripe/rainier/sampler/Sampler.scala
+++ rainier-core/src/main/scala/com/stripe/rainier/sampler/Sampler.scala
@@ -1,6 +1,7 @@
 package com.stripe.rainier.sampler
 
 import com.stripe.rainier.compute._
+import scala.annotation.tailrec
 
 trait Sampler {
   def sample(density: Real, warmupIterations: Int)(
@@ -8,6 +9,77 @@ trait Sampler {
 }
 
 final case class Sample(accepted: Boolean, parameters: Array[Double])
+final case class Diagnostics(rHat: Double, effectiveSampleSize: Double)
+
+object Diagnostics {
+  def apply(traces: Seq[Array[Double]]): Diagnostics = {
+    val m = traces.size.toDouble
+    val n = traces.head.size.toDouble
+    val (rHat, v) = rHatAndV(traces, n, m)
+    val ac = autocorrelation(traces, n, m, v, 1, 0.0)
+    val ess = n * m / (1 + (2 * ac))
+    Diagnostics(rHat, ess)
+  }
+
+  def rHatAndV(traces: Seq[Array[Double]],
+               n: Double,
+               m: Double): (Double, Double) = {
+
+    val means = traces.map { t =>
+      t.sum / n
+    }
+
+    val meanMean = means.sum / n
+
+    val b = (n / (m - 1)) * means.map { m =>
+      Math.pow(m - meanMean, 2)
+    }.sum
+
+    val variances =
+      traces.zip(means).map {
+        case (t, m) =>
+          t.map { a =>
+            Math.pow(a - m, 2)
+          }.sum / (n - 1)
+      }
+
+    val w = variances.sum / m
+
+    val v =
+      (n - 1).toDouble / n * w +
+        b / n
+
+    val rHat = Math.sqrt(v / w)
+    (rHat, v)
+  }
+
+  @tailrec
+  def autocorrelation(traces: Seq[Array[Double]],
+                      n: Double,
+                      m: Double,
+                      v: Double,
+                      lag: Int,
+                      acc: Double): Double = {
+    val vt = traces.map { trace =>
+      variogram(trace, lag)
+    }.sum / m
+    val pt = 1.0 - (vt / (2.0 * v))
+    if (pt > 0.0 && lag < 100)
+      autocorrelation(traces, n, m, v, lag + 1, acc + pt)
+    else
+      acc
+  }
+
+  def variogram(trace: Array[Double], lag: Int): Double = {
+    var i = lag
+    var sum = 0.0
+    while (i < trace.size) {
+      sum += Math.pow(trace(i) - trace(i - lag), 2)
+      i += 1
+    }
+    sum / (trace.size - lag).toDouble
+  }
+}
 
 object Sampler {
   object Default {
@@ -16,33 +88,15 @@ object Sampler {
     val warmupIterations: Int = 10000
   }
 
-  def gelmanRubin(chains: List[List[Array[Double]]]): List[Double] = {
-    val nChains = chains.size
-    val nSamples = chains.head.size
+  def diagnostics(chains: List[List[Array[Double]]]): List[Diagnostics] = {
     val nParams = chains.head.head.size
     0.until(nParams).toList.map { i =>
       val traces = chains.map { c =>
         c.map { a =>
           a(i)
-        }
+        }.toArray
       }
-      val means = traces.map { t =>
-        t.sum / nSamples
-      }
-      val variances =
-        traces.zip(means).map {
-          case (t, m) =>
-            t.map { a =>
-              Math.pow(a - m, 2)
-            }.sum / (nSamples - 1)
-        }
-      val w = variances.sum / nChains
-      val meanMean = means.sum / nChains
-      val b = means.map { m =>
-        Math.pow(m - meanMean, 2)
-      }.sum / (nChains - 1)
-      val v = (1.0 - (1.0 / nSamples)) * w + b
-      Math.sqrt(v / w)
+      Diagnostics(traces)
     }
   }
 }
