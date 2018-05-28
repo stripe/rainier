61ce81cbe16255f983e9da106966bf1165d81dd3
Jorge Montero <jmontero@cibotechnologies.com>
2018-05-27 21:15:22 -0500

    basic evilplot implementation

--- rainier-core/src/main/scala/com/stripe/rainier/repl/package.scala
+++ rainier-core/src/main/scala/com/stripe/rainier/repl/package.scala
@@ -1,5 +1,9 @@
 package com.stripe.rainier
 
+import java.io.File
+
+import com.cibo.evilplot.numeric.Point
+import com.cibo.evilplot.plot.ContourPlot
 import com.stripe.rainier.sampler._
 
 package object repl {
@@ -13,6 +17,19 @@ package object repl {
       DensityPlot()
         .plot2D(seq.map { case (a, b) => (m.toDouble(a), n.toDouble(b)) })
         .mkString("\n"))
+  }
+
+  def contourPlot[M, N](seq: Seq[(M, N)], file: String, contours: Int = 5)(
+      implicit n: Numeric[N],
+      m: Numeric[M]): Unit = {
+    val points = seq.map {
+      case (x, y) => Point(m.toDouble(x), n.toDouble((y)))
+    }
+
+    import com.cibo.evilplot.plot.aesthetics.DefaultTheme.defaultTheme
+    ContourPlot(points, contours = Some(contours))
+      .render()
+      .write(new File(file))
   }
 
   implicit val rng: RNG = RNG.default
