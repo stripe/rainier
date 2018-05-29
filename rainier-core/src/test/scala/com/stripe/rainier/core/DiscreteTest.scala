83f02abb2927054160b95e990b3ff84ed6c9a969
Avi Bryant <avi@stripe.com>
2018-05-29 08:42:36 -0700

    fix test seed

--- rainier-core/src/test/scala/com/stripe/rainier/core/DiscreteTest.scala
+++ rainier-core/src/test/scala/com/stripe/rainier/core/DiscreteTest.scala
@@ -5,7 +5,7 @@ import com.stripe.rainier.sampler._
 import org.scalatest.FunSuite
 
 class DiscreteTest extends FunSuite {
-  implicit val rng: RNG = ScalaRNG(1527607304199L)
+  implicit val rng: RNG = ScalaRNG(1527608515939L)
 
   def check[N: Numeric](description: String)(
       fn: Real => Distribution[N]): Unit = {
