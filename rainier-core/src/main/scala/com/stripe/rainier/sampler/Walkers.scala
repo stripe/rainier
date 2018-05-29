f0e800403b9cc2eaa574270621f9c9730340a585
Avi Bryant <avi@stripe.com>
2018-05-29 08:22:39 -0700

    keepEvery for walkers

--- rainier-core/src/main/scala/com/stripe/rainier/sampler/Walkers.scala
+++ rainier-core/src/main/scala/com/stripe/rainier/sampler/Walkers.scala
@@ -1,6 +1,7 @@
 package com.stripe.rainier.sampler
 
 import com.stripe.rainier.compute._
+import scala.collection.mutable.ListBuffer
 
 final case class Walkers(walkers: Int) extends Sampler {
   def sample(density: Real,
@@ -14,13 +15,17 @@ final case class Walkers(walkers: Int) extends Sampler {
           case (chain, _) =>
             chain.next
         }
-    //TODO: respect keepEvery
-    1.to(iterations)
-      .scanLeft(warmedUp) {
-        case (chain, _) =>
-          chain.next
-      }
-      .map(_.variables)
-      .toList
+
+    val buf = new ListBuffer[Array[Double]]
+    var i = 0
+    var chain = warmedUp
+    while (i < iterations) {
+      chain = chain.next
+      //keep every nth state of each walker, vs every nth walker
+      if ((i / walkers) % keepEvery == 0)
+        buf += chain.variables
+      i += 1
+    }
+    buf.toList
   }
 }
