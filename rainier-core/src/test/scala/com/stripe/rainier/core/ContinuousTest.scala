d98920e7544c00c8f3691ba85832a63b3f1cc2f5
Avi Bryant <avi@stripe.com>
2018-05-30 16:01:48 -0700

    fix test seed

--- rainier-core/src/test/scala/com/stripe/rainier/core/ContinuousTest.scala
+++ rainier-core/src/test/scala/com/stripe/rainier/core/ContinuousTest.scala
@@ -5,7 +5,7 @@ import com.stripe.rainier.sampler._
 import org.scalatest.FunSuite
 
 class ContinuousTest extends FunSuite {
-  implicit val rng: RNG = ScalaRNG(1527717507662L)
+  implicit val rng: RNG = ScalaRNG(1527721207572L)
 
   def check(description: String)(fn: Real => Continuous): Unit = {
     println(description)
