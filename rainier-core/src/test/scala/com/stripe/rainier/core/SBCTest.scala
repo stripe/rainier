385e9e794b6a8adbb11b961fee020dace9f6cee0
Avi Bryant <avi@stripe.com>
2018-06-05 08:30:12 -0700

    wip

--- rainier-core/src/test/scala/com/stripe/rainier/core/SBCTest.scala
+++ rainier-core/src/test/scala/com/stripe/rainier/core/SBCTest.scala
@@ -4,26 +4,39 @@ import com.stripe.rainier.compute._
 import com.stripe.rainier.sampler._
 import org.scalatest.FunSuite
 
-class SBCTest extends FunSuite {
-  implicit val rng: RNG = ScalaRNG(123L)
-
-  def check(description: String)(run: SBCRun) = {
-    run.animate(3)
-    //when this test fails, if the calibration looked good,
-    //plug the rHat value listed in the output into the call
-    assert(false)
+trait SBCTest[T] {
+  def sbc: SBC[T]
+  def samplers: List[(Sampler, Int, Int)]
+  def main(args: Array[String]): Unit = {
+    samplers.foreach {
+      case (s, wi, sd) =>
+        println(s"$s with $wi warmup iterations and $sd synthetic data points")
+        implicit val rng = ScalaRNG(123L)
+        sbc.prepare(s, wi, sd).animate(3)
+    }
   }
+}
 
-  def check(description: String, rHat: Double)(run: SBCRun) = {
-    //when this fails, delete the rHat parameter to see a new run
-    assert(rHat == run.rHat)
+trait SBCSuite extends FunSuite {
+  def check[T](sbc: SBC[T],
+               samplers: List[(Sampler, Int, Int)],
+               rHats: List[Double]) = {
+    samplers.zip(rHats).foreach {
+      case ((s, wi, sd), rHat) =>
+        test(s"$s with $wi warmup iterations and $sd synthetic data points") {
+          implicit val rng = ScalaRNG(123L)
+          val run = sbc.prepare(s, wi, sd)
+          assert(run.rHat == rHat)
+        }
+    }
   }
+}
 
-  val normal = SBC(LogNormal(0, 1)) { sd =>
+object NormalSBCTest extends SBCTest[Double] {
+  val sbc = SBC(LogNormal(0, 1)) { sd =>
     Normal(0, sd)
   }
-
-  check("Normal with HMC") {
-    normal.prepare(HMC(5), 1000, 1000)
-  }
+  val samplers = List(
+    (HMC(5), 1000, 1000)
+  )
 }
