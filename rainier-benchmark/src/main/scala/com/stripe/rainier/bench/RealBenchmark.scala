ca88aa0cbea58fee955aa5502724ff9fae80d4db
Avi Bryant <avi@stripe.com>
2018-06-05 16:59:57 -0700

    standardize benchmarks

---
+++ rainier-benchmark/src/main/scala/com/stripe/rainier/bench/RealBenchmark.scala
@@ -0,0 +1,155 @@
+package com.stripe.rainier.bench
+
+import org.openjdk.jmh.annotations._
+import java.util.concurrent.TimeUnit
+
+import com.stripe.rainier.compute._
+import com.stripe.rainier.core._
+import com.stripe.rainier.sampler._
+
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 3, time = 1)
+@Measurement(iterations = 5, time = 1)
+@Fork(3)
+@Threads(4)
+@State(Scope.Benchmark)
+abstract class RealBenchmark {
+  implicit val rng: RNG = RNG.default
+
+  protected def expression: Real
+
+  val expr = expression
+  val vars = expr.variables
+  val grad = gradient
+  val cf = compile
+  val gf = compileGradient
+
+  @Benchmark
+  def build = expression
+  @Benchmark
+  def gradient = expr.gradient
+  @Benchmark
+  def compile = Compiler.default.compile(vars, expr)
+  @Benchmark
+  def compileGradient = Compiler.default.compile(vars, grad)
+  @Benchmark
+  def run =
+    cf(vars.map { _ =>
+      rng.standardUniform
+    }.toArray)
+  @Benchmark
+  def runGradient =
+    gf(vars.map { _ =>
+      rng.standardUniform
+    }.toArray)
+}
+
+class TrivialBenchmark extends RealBenchmark {
+  def expression = Real(3)
+}
+
+class NormalBenchmark extends RealBenchmark {
+  def expression: Real = {
+    val x = new Variable
+    Normal(x, 1).logDensities(
+      Range.BigDecimal(0d, 2d, 0.001d).map(_.toDouble).toList)
+  }
+}
+
+class PoissonBenchmark extends RealBenchmark {
+  def expression: Real = {
+    val x = new Variable
+    Poisson(x).logDensities(0.to(10).toList)
+  }
+}
+
+class FullNormalBenchmark extends RealBenchmark {
+  def expression: Real = {
+    val r = new scala.util.Random
+    val trueMean = 3.0
+    val trueStddev = 2.0
+    val data = 1.to(1000).map { i =>
+      (r.nextGaussian * trueStddev) + trueMean
+    }
+
+    val model = for {
+      mean <- Uniform(0, 10).param
+      stddev <- Uniform(0, 10).param
+      _ <- Normal(mean, stddev).fit(data)
+    } yield (mean, stddev)
+
+    model.density
+  }
+}
+
+class BernoulliBenchmark extends RealBenchmark {
+  def expression: Real = {
+    val data =
+      List(false, true, false, false, false, false, false, false, false, true)
+
+    val model = for {
+      theta <- Uniform.standard.param
+      _ <- Categorical.boolean(theta).fit(data)
+    } yield theta
+
+    model.density
+  }
+}
+
+class FunnelBenchmark extends RealBenchmark {
+  def expression: Real = {
+    val model =
+      for {
+        y <- Normal(0, 3).param
+        x <- RandomVariable.traverse(1.to(9).map { _ =>
+          Normal(0, (y / 2).exp).param
+        })
+      } yield (x(0), y)
+
+    model.density
+  }
+}
+
+class DLMBenchmark extends RealBenchmark {
+  def expression: Real = {
+    implicit val rng = ScalaRNG(4)
+    val n = 50
+    val mu = 3.0 // AR(1) mean
+    val a = 0.95 // auto-regressive parameter
+    val sig = 1.0 // AR(1) SD
+    val sigD = 3.0 // observational SD
+    val state = Stream
+      .iterate(0.0)(x => mu + (x - mu) * a + sig * rng.standardNormal)
+      .take(n)
+      .toVector
+    val obs = state.map(_ + sigD * rng.standardNormal)
+
+    // build and fit model
+    case class Static(mu: Real, a: Real, sig: Real, sigD: Real)
+
+    val prior = for {
+      mu <- Normal(5, 10).param
+      a <- Normal(1, 0.2).param
+      sig <- LogNormal(0, 2).param
+      sigD <- LogNormal(1, 4).param
+      sp <- Normal(0, 50).param
+    } yield (Static(mu, a, sig, sigD), List(sp))
+
+    def addTimePoint(current: RandomVariable[(Static, List[Real])],
+                     i: Int): RandomVariable[(Static, List[Real])] =
+      for {
+        tup <- current
+        static = tup._1
+        states = tup._2
+        os = states.head
+        ns <- Normal(((Real.one - static.a) * static.mu) + (static.a * os),
+                     static.sig).param
+        _ <- Normal(ns, static.sigD).fit(obs(i))
+      } yield (static, ns :: states)
+
+    val fullModel = (0 until n).foldLeft(prior)(addTimePoint(_, _))
+
+    fullModel.density
+  }
+}
