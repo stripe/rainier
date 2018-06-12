4df72657b3b6ed30f484c44e80989f41d574aac1
Jonathan Glidden <jglidden@stripe.com>
2018-06-11 18:27:37 -0700

    some more examples

--- rainier-core/src/test/scala/rainier/sampler/VariationalTest.scala
+++ rainier-core/src/test/scala/rainier/sampler/VariationalTest.scala
@@ -12,7 +12,25 @@ class VariationalTest extends FunSuite {
     val x = Normal(2.0, 3.0).param
     val variationalSampler = new Variational(0.0, 0, 100, 100, .1)
     val samples = x.sample(variationalSampler, 1, 100000, 1)
-    println(samples.take(10))
     plot1D(samples)
   }
+
+  test(" 2d independant normal") {
+    val x = Normal(3.0, 0.4).param
+    val y = x.flatMap(a => Normal(1.5, 0.2).param)
+    val xy = x.zip(y)
+    val variationalSampler = new Variational(0.0, 0, 100, 100, .1)
+    val samples = xy.sample(variationalSampler, 1, 100000, 1)
+    plot2D(samples)
+  }
+
+  test(" 2d normal") {
+    val x = Normal(2.0, 1.0).param
+    val y = x.flatMap(a => Normal(a, 0.3).param)
+    val xy = x.zip(y)
+    val variationalSampler = new Variational(0.0, 0, 100, 100, .1)
+    val samples = xy.sample(variationalSampler, 1, 100000, 1)
+    plot2D(samples)
+  }
+
 }
