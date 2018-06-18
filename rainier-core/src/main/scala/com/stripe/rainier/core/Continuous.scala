4698351882c2bfb128f1a2a975e5e44538e3cef2
Avi Bryant <avi@stripe.com>
2018-06-17 21:20:06 -0700

    wip

--- rainier-core/src/main/scala/com/stripe/rainier/core/Continuous.scala
+++ rainier-core/src/main/scala/com/stripe/rainier/core/Continuous.scala
@@ -7,17 +7,12 @@ import scala.annotation.tailrec
 /**
   * A Continuous Distribution, with method `param` allowing conversion to a RandomVariable.
   */
-trait Continuous extends Distribution[Double] {
-  def logDensity(t: Double): Real =
-    realLogDensity(Real(t))
+trait Continuous extends Distribution[Double,Real] {
+  def param: RandomVariable[Real]
 
   def scale(a: Real): Continuous = Scale(a).transform(this)
   def translate(b: Real): Continuous = Translate(b).transform(this)
   def exp: Continuous = Exp.transform(this)
-
-  private[rainier] def realLogDensity(real: Real): Real
-
-  def param: RandomVariable[Real]
 }
 
 /**
@@ -51,7 +46,7 @@ trait LocationScaleFamily { self =>
       Generator.from { (r, n) =>
         generate(r)
       }
-    def realLogDensity(real: Real): Real =
+    def logDensity(real: Real): Real =
       self.logDensity(real)
   }
 
@@ -100,7 +95,7 @@ object Gamma {
   def standard(shape: Real): Continuous = new StandardContinuous {
     val support = PositiveSupport
 
-    def realLogDensity(real: Real): Real =
+    def logDensity(real: Real): Real =
       If(real > 0,
          (shape - 1) * real.log -
            Combinatorics.gamma(shape) - real,
@@ -154,7 +149,7 @@ object Exponential {
 final case class Beta(a: Real, b: Real) extends StandardContinuous {
   val support = OpenUnitSupport
 
-  def realLogDensity(real: Real): Real =
+  def logDensity(real: Real): Real =
     If(real >= 0,
        If(real <= 1, betaDensity(real), Real.negInfinity),
        Real.negInfinity)
@@ -198,8 +193,8 @@ object Uniform {
   val standard: Continuous = new StandardContinuous {
     val support = beta11.support
 
-    def realLogDensity(real: Real): Real = beta11.realLogDensity(real)
-
+    def logDensity(real: Real): Real = beta11.logDensity(real)
+    def param: RandomVariable[Real] = beta11.param
     val generator: Generator[Double] =
       Generator.from { (r, n) =>
         r.standardUniform
