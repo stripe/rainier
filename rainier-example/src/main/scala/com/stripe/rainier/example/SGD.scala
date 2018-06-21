1102c4bdd2db7e9060fe78a3eca01b966d9dfe82
Avi Bryant <avi@stripe.com>
2018-06-21 13:04:54 -0700

    working SGD example

--- rainier-example/src/main/scala/com/stripe/rainier/example/SGD.scala
+++ rainier-example/src/main/scala/com/stripe/rainier/example/SGD.scala
@@ -11,10 +11,38 @@ object SGD {
     for {
       a <- LogNormal(0, 1).param
       b <- LogNormal(0, 1).param
-    //  _ <- data.fit(Predictor.from { k: Real =>
-        BetaBinomial(a, b, k)
-      })
+      _ <- fit(data, 100) { (k, n) =>
+        BetaBinomial.logDensity(a, b, k, n)
+      }
     } yield (a, b)
+
+  //super hacky and manual
+  def fit(data: List[(Int, Int)], batchSize: Int)(
+      fn: (Real, Real) => Real): RandomVariable[Unit] = {
+    val batchVariables = 1.to(batchSize).map { _ =>
+      (new Variable, new Variable)
+    }
+    val density = Real.sum(batchVariables.map(fn.tupled))
+    val buffers = 1.to(batchSize).map { _ =>
+      (new scala.collection.mutable.ArrayBuffer[Double],
+       new scala.collection.mutable.ArrayBuffer[Double])
+    }
+    data.grouped(batchSize).foreach { batch =>
+      if (batch.size == batchSize) {
+        batch.zip(buffers).foreach {
+          case ((k, n), (kBuf, nBuf)) =>
+            kBuf += k.toDouble
+            nBuf += n.toDouble
+        }
+      }
+    }
+    val columns =
+      batchVariables.zip(buffers).flatMap {
+        case ((kVar, nVar), (kBuf, nBuf)) =>
+          List((kVar, kBuf.toArray), (nVar, nBuf.toArray))
+      }
+    RandomVariable((), density, columns.toMap)
+  }
 
   def main(args: Array[String]): Unit = {
     val data: List[(Int, Int)] =
