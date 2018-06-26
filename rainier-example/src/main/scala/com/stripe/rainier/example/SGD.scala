8e27c5e4c33b9a8f25944fd25770c23cefd748ed
Avi Bryant <avi@stripe.com>
2018-06-26 09:30:36 -0700

    SGD with synthetic data

--- rainier-example/src/main/scala/com/stripe/rainier/example/SGD.scala
+++ rainier-example/src/main/scala/com/stripe/rainier/example/SGD.scala
@@ -4,17 +4,23 @@ import com.stripe.rainier.compute._
 import com.stripe.rainier.core._
 import com.stripe.rainier.sampler
 import com.stripe.rainier.repl._
-import scala.io.Source
 
 object SGD {
+  def hmcModel(data: List[(Int, Int)]): RandomVariable[(Real, Real)] =
+    for {
+      u <- Uniform(0, 1).param
+      v <- Exponential(0.1).param
+      _ <- Beta(u * v, (1 - u) * v).binomial.fit(data)
+    } yield (u, v)
+
   def model(data: List[(Int, Int)]): RandomVariable[(Real, Real)] =
     for {
-      a <- LogNormal(0, 1).param
-      b <- LogNormal(0, 1).param
+      u <- Uniform(0, 1).param
+      v <- Exponential(0.1).param
       _ <- fit(data, 100) { (k, n) =>
-        BetaBinomial.logDensity(a, b, k, n)
+        BetaBinomial.logDensity(u * v, (1 - u) * v, k, n)
       }
-    } yield (a, b)
+    } yield (u, v)
 
   //super hacky and manual
   def fit(data: List[(Int, Int)], batchSize: Int)(
@@ -45,19 +51,24 @@ object SGD {
   }
 
   def main(args: Array[String]): Unit = {
+    val trueU = 0.3
+    val trueV = 8
+
     val data: List[(Int, Int)] =
-      Source
-        .fromFile(args(0))
-        .getLines
-        .map { line =>
-          val parts = line.split("\t")
-          (parts(0).toInt, parts(1).toInt)
+      RandomVariable(Exponential(0.01).generator.map(_.toInt).flatMap { k =>
+        BetaBinomial(trueU * trueV, (1.0 - trueU) * trueV, k).generator.map {
+          n =>
+            (k, n)
         }
-        .toList
+      }).sample(100000)
 
     val m = model(data)
 
-    val (a, b) = m.optimize(sampler.SGD(0.001), 5)
-    println(s"Beta($a,$b)")
+    val (u, v) = m.optimize(sampler.SGD(0.00001), 20)
+    println((u, v))
+
+    val m2 = hmcModel(data.take(1000))
+    val samples = m2.sample(sampler.HMC(200), 1000, 1000)
+    plot2D(samples)
   }
 }
