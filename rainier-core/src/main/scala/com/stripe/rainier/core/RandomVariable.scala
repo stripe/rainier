f7953b20cc6b7bc9c0cf44c0c991d1e79b660f1d
Avi Bryant <avi@stripe.com>
2018-07-31 16:51:16 -0700

    mark unused

--- rainier-core/src/main/scala/com/stripe/rainier/core/RandomVariable.scala
+++ rainier-core/src/main/scala/com/stripe/rainier/core/RandomVariable.scala
@@ -2,6 +2,7 @@ package com.stripe.rainier.core
 
 import com.stripe.rainier.compute._
 import com.stripe.rainier.sampler._
+import com.stripe.rainier.unused
 
 /**
   * The main probability monad used in Rainier for constructing probabilistic programs which can be sampled
@@ -45,7 +46,7 @@ class RandomVariable[+T](val value: T,
   def record(sampler: Sampler,
              warmupIterations: Int,
              iterations: Int,
-             batches: Int = 1,
+             @unused batches: Int = 1,
              keepEvery: Int = 1)(implicit rng: RNG): Recording = {
     val posteriorParams = Sampler
       .sample(Context(density),
@@ -85,7 +86,7 @@ class RandomVariable[+T](val value: T,
   def sample[V](sampler: Sampler,
                 warmupIterations: Int,
                 iterations: Int,
-                batches: Int = 1,
+                @unused batches: Int = 1,
                 keepEvery: Int = 1)(implicit rng: RNG,
                                     sampleable: Sampleable[T, V]): List[V] = {
     val context = Context(density)
@@ -102,7 +103,7 @@ class RandomVariable[+T](val value: T,
                                warmupIterations: Int,
                                iterations: Int,
                                parallel: Boolean = true,
-                               batches: Int = 1,
+                               @unused batches: Int = 1,
                                keepEvery: Int = 1)(
       implicit rng: RNG,
       sampleable: Sampleable[T, V]): (List[V], List[Diagnostics]) = {
