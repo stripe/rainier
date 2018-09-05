742238269c2b2b63c5f3c36e75b9a6a74718bcda
Mio Alter <mio@stripe.com>
2018-09-05 19:30:33 -0400

    still noodling

--- rainier-core/src/main/scala/com/stripe/rainier/core/SBCModel.scala
+++ rainier-core/src/main/scala/com/stripe/rainier/core/SBCModel.scala
@@ -2,34 +2,31 @@ package com.stripe.rainier.core
 
 import com.stripe.rainier.compute._
 import com.stripe.rainier.sampler._
-//import com.stripe.rainier.repl._
+import com.stripe.rainier.repl._
 
-/**
-  * Notes to self:
-  * - without extends App, Scala does not recognize UniformNormal as having
-  *   a main method
-  * - with it, it does, but doesn't actually run it when it is selected.
-  * - putting all of the SBCModels in the SBCModel object, they were also not
-  *   recognized
-  * - if we _do_ find a way to get Scala to recognize main methods, maybe we can
-  *   define everything in rainierTests.
-  */
 trait SBCModel[L, T] {
+  implicit val rng: RNG = ScalaRNG(1528673302081L)
   def sbc: SBC[L, T]
-  def main(args: Array[String])(implicit rng: RNG): Unit =
+  def main(args: Array[String]): Unit =
     sbc.animate(HMC(1), 10000, 1000)
-  def samples(nSamples: Int)(
-      implicit rng: RNG,
-      sampleable: Sampleable[(L, Real), Double]): List[Double] =
-    sbc.posterior.sample(nSamples)
+//  def samples(nSamples: Int)(
+//      implicit sampleable: Sampleable[(L, Real), Double]): List[Double] =
+//    sbc.posterior.sample(nSamples)
+  def samples(nSamples: Int): List[Double]
+  def goldsetSamples: List[Double]
+  def agreesWithGoldset: Boolean =
+    samples(goldsetSamples.size) == goldsetSamples
   def description: String
 }
 
-case object UniformNormal extends SBCModel[Continuous, Double] with App {
+object SBCUniformNormal extends SBCModel[Continuous, Double] {
   def sbc = SBC(Uniform(0, 1))((x: Real) => Normal(x, 1))
+  def samples(nSamples: Int) = List(0.13, 0.11, 2.223)
+  def goldsetSamples: List[Double] = List(0.1, 0.33, 1.12)
   def description = "x => Normal(x,1) with Uniform(0,1) prior"
 }
 
-case object Yo {
-  def main(args: Array[String]): Unit = println("yo")
-}
+//object LogNormal extends SBCModel[Continuous, Double] {
+//  def sbc = SBC(LogNormal(0,1))((x: Real) => LogNormal(x,x))
+//  def description = "x => LogNormal(x,x) with LogNormal(0,1) prior"
+//}
--- rainier-tests/src/test/scala/com/stripe/rainier/core/SBCTest.scala
+++ rainier-tests/src/test/scala/com/stripe/rainier/core/SBCTest.scala
@@ -1,20 +1,17 @@
 package com.stripe.rainier.core
 
-//import com.stripe.rainier.compute._
-//import com.stripe.rainier.sampler._
 import org.scalatest.FunSuite
 
-/** This is what happens when we make the test extend App
- * [warn] Multiple main classes detected.  Run 'show discoveredMainClasses' to see the list
- * [error] java.lang.RuntimeException: No main class detected.
- * running discoveredMainClasses returns no main classes detected
- */
-object SBCTest extends FunSuite {
+class SBCTest extends FunSuite {
 
-  implicit val rng: RNG = ScalaRNG(1528673302081L)
-  def check[L, T](goldsetSamples: List[Double], sbcModel: SBCModel[L, T]) = {
+  def check[L, T](sbcModel: SBCModel[L, T]): Unit = {
+    println(sbcModel.description)
     test(sbcModel.description) {
-      assert(sbcModel.samples(goldsetSamples.size) == goldsetSamples)
+      assert(sbcModel.agreesWithGoldset)
     }
   }
+
+  check(SBCUniformNormal)
+
 }
+
