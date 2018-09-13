c45cee739a2a685aa1c29003bebec9d99fe6086b
Mio Alter <mio@stripe.com>
2018-09-13 12:12:46 -0400

    add big + small + ... + small test for Kahan summation

--- rainier-tests/src/test/scala/com/stripe/rainier/compute/KahanTest.scala
+++ rainier-tests/src/test/scala/com/stripe/rainier/compute/KahanTest.scala
@@ -41,4 +41,37 @@ class KahanTest extends FunSuite {
     bigAndSmallExpected
   )
 
+  /**
+    * Given an array of the form  Array(big, small, small),
+    * the naive sum computes (big + small) + small
+    * where the first sum truncates low-order bits and the second sum truncates
+    * again. The best we can do is to compute
+    * big + (small + small)
+    * so that we keep all of the bits we can until the final sum which truncates
+    * once.
+    * In the below example
+    * array.sum = 1.0000000065973433E10
+    * whereas
+    * big + (sum of smalls) = Kahan.sum(array) = 1.0000000065973446E10
+    */
+  val big = 1E10
+  val small = Math.PI
+  val nSmall = 21
+  val oneBigManySmallArray = big +: Array.fill(nSmall)(small)
+  val oneBigManySmallExpected = big + (nSmall * small)
+
+  check(
+    "Kahan.sum computes big + (small + ... + small) for an Array(big, small...., small)")(
+    Kahan.sum,
+    oneBigManySmallArray,
+    oneBigManySmallExpected
+  )
+
+  check(
+    "Kahan.nSum computes big + (small + ... + small) for an Array(big, small...., small)")(
+    Kahan.nSum,
+    oneBigManySmallArray,
+    oneBigManySmallExpected
+  )
+
 }
