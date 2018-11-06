186a9da5b215bafd5d7e9b64b4bbad9bbd5068d4
Avi Bryant <avi@stripe.com>
2018-11-06 10:41:06 -0800

    fix comparison order

--- rainier-core/src/main/scala/com/stripe/rainier/core/Categorical.scala
+++ rainier-core/src/main/scala/com/stripe/rainier/core/Categorical.scala
@@ -99,7 +99,7 @@ object Multinomial {
     Combinatorics.factorial(multi.k) + Real.sum(v.toList.map {
       case (t, i) =>
         val p = multi.pmf.getOrElse(t, Real.zero)
-        val pTerm = 
+        val pTerm =
           Real.eq(i, Real.zero, Real.zero, i * p.log)
         pTerm - Combinatorics.factorial(i)
     })
