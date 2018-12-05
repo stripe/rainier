bcdc410ff6b932eeadf820f48098c295fc1d7b2a
Avi Bryant <avi@avibryant.com>
2018-12-05 13:38:35 -0800

    renamings for mio

--- rainier-core/src/main/scala/com/stripe/rainier/core/Predictor.scala
+++ rainier-core/src/main/scala/com/stripe/rainier/core/Predictor.scala
@@ -5,13 +5,13 @@ import com.stripe.rainier.compute._
 /**
   * Predictor class, for fitting data with covariates
   */
-sealed trait Predictor[L, X] { self =>
+sealed trait Predictor[X, L] { self =>
   type P
-  protected def xp: Encoder[X, P]
+  protected def encoder: Encoder[X, P]
   protected def create(p: P): L
 
   def fit[Y](values: Seq[(X, Y)])(
-      implicit lh: ToLikelihood[L, Y]): RandomVariable[Predictor[L, X]] =
+      implicit lh: ToLikelihood[L, Y]): RandomVariable[Predictor[X, L]] =
     Predictor
       .likelihood[L, X, Y](this)
       .fit(values)
@@ -20,7 +20,7 @@ sealed trait Predictor[L, X] { self =>
       }
 
   def predict[Y](x: X)(implicit gen: ToGenerator[L, Y]): Generator[Y] =
-    gen(create(xp.wrap(x)))
+    gen(create(encoder.wrap(x)))
 
   def predict[Y](seq: Seq[X])(
       implicit gen: ToGenerator[L, Y]): Generator[Seq[(X, Y)]] =
@@ -32,38 +32,38 @@ sealed trait Predictor[L, X] { self =>
 }
 
 object Predictor extends LikelihoodMaker {
-  def likelihood[L, X, Y](pred: Predictor[L, X])(
+  def likelihood[L, X, Y](pred: Predictor[X, L])(
       implicit lh: ToLikelihood[L, Y]): Likelihood[(X, Y)] = {
-    val (p, v) = pred.xp.create(Nil)
+    val (p, vs) = pred.encoder.create(Nil)
     val l = pred.create(p)
     val inner = lh(l)
     new Likelihood[(X, Y)] {
       val real = inner.real
-      val placeholders = v ++ inner.placeholders
+      val placeholders = vs ++ inner.placeholders
       def extract(t: (X, Y)) =
-        pred.xp.extract(t._1, Nil) ++ inner.extract(t._2)
+        pred.encoder.extract(t._1, Nil) ++ inner.extract(t._2)
     }
   }
 
-  class Maker[X, A](xa: Encoder[X, A]) {
-    def apply[B](fn: A => B): Predictor[B, X] =
-      new Predictor[B, X] {
+  class Maker[X, A](enc: Encoder[X, A]) {
+    def apply[B](fn: A => B): Predictor[X, B] =
+      new Predictor[X, B] {
         type P = A
-        val xp = xa
+        val encoder = enc
         def create(p: P) = fn(p)
       }
   }
 
   type M[X, A] = Maker[X, A]
-  def maker[X, A](implicit xa: Encoder[X, A]) = new Maker(xa)
+  def maker[X, A](implicit enc: Encoder[X, A]) = new Maker(enc)
 
-  class Fitter[X, A, Y](seq: Seq[(X, Y)], xa: Encoder[X, A]) {
-    val m = maker(xa)
+  class Fitter[X, A, Y](seq: Seq[(X, Y)], enc: Encoder[X, A]) {
+    val m = maker(enc)
     def to[B](fn: A => B)(
-        implicit lh: ToLikelihood[B, Y]): RandomVariable[Predictor[B, X]] =
+        implicit lh: ToLikelihood[B, Y]): RandomVariable[Predictor[X, B]] =
       m(fn).fit(seq)
   }
 
-  def fit[X, Y, A, B](seq: Seq[(X, Y)])(implicit xa: Encoder[X, A]) =
-    new Fitter(seq, xa)
+  def fit[X, Y, A, B](seq: Seq[(X, Y)])(implicit enc: Encoder[X, A]) =
+    new Fitter(seq, enc)
 }
