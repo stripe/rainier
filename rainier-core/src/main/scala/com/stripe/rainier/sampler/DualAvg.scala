6ea1e3c37501ee44e40c2f8bc55058f19ffc1815
Avi Bryant <avi@stripe.com>
2019-07-16 20:28:11 -0700

    formatting

--- rainier-core/src/main/scala/com/stripe/rainier/sampler/DualAvg.scala
+++ rainier-core/src/main/scala/com/stripe/rainier/sampler/DualAvg.scala
@@ -81,7 +81,8 @@ private object DualAvg {
   private def computeExponent(logAcceptanceProb: Double): Double =
     if (logAcceptanceProb > Math.log(0.5)) { 1.0 } else { -1.0 }
 
-  private def continueTuningStepSize(stepSize: Double, logAcceptanceProb: Double,
+  private def continueTuningStepSize(stepSize: Double,
+                                     logAcceptanceProb: Double,
                                      exponent: Double): Boolean =
     stepSize != 0.0 && (exponent * logAcceptanceProb > -exponent * Math.log(2))
 
