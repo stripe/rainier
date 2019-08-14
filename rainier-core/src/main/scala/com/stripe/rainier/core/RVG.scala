8b807c45c80f2a84ffdc54d0d551317112c3e6a9
avi-stripe <avi@stripe.com>
2019-08-14 11:46:55 -0700

    bare bones RVG (#354)

---
+++ rainier-core/src/main/scala/com/stripe/rainier/core/RVG.scala
@@ -0,0 +1,21 @@
+package com.stripe.rainier.core
+
+case class RVG[T](value: RandomVariable[Generator[T]]) {
+  def map[U](fn: T => U): RVG[U] = RVG(value.map(_.map(fn)))
+
+  def zip[U](other: RVG[U]): RVG[(T, U)] =
+    RVG(value.zip(other.value).map { case (tg, ug) => tg.zip(ug) })
+
+  def flatMapGen[U](fn: T => Generator[U]): RVG[U] =
+    RVG(value.map(_.flatMap(fn)))
+}
+
+object RVG {
+  def traverse[T](seq: Seq[RVG[T]]): RVG[Seq[T]] =
+    RVG(
+      RandomVariable
+        .traverse(seq.map(_.value))
+        .map { gs =>
+          Generator.traverse(gs)
+        })
+}
