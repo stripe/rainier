6a2372bd454bcbf637399c60ce68572ed79af70d
Avi Bryant <avi@stripe.com>
2019-08-28 22:36:09 -0700

    test against orig

--- rainier-core/src/main/scala/com/stripe/rainier/optimizer/Optimizer.scala
+++ rainier-core/src/main/scala/com/stripe/rainier/optimizer/Optimizer.scala
@@ -7,8 +7,15 @@ object Optimizer {
     val x = new Array[Double](df.nVars)
     val g = new Array[Double](df.nVars)
 
+    //for LBFGSOrig
+    val x2 = new Array[Double](df.nVars)
+    val diag = new Array[Double](df.nVars)
+    val iflag = Array(0)
+
     var complete = false
-    val lb = new LBFGS(x, 5, 0.1)
+    val m = 5
+    val eps = 0.1
+    val lb = new LBFGS(x, m, eps)
     while (!complete) {
       df.update(x)
       var i = 0
@@ -17,6 +24,17 @@ object Optimizer {
         i += 1
       }
       complete = lb(df.density * -1, g)
+
+      LBFGSOrig.lbfgs(x2.size,
+                      m,
+                      x2,
+                      df.density * -1,
+                      g,
+                      false,
+                      diag,
+                      eps,
+                      iflag)
+      assert(x.toList == x2.toList)
     }
     return x
   }
