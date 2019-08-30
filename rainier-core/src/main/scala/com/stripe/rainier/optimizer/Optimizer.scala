e7a9a0d7842b1750a1f752195c569b9b979993c3
Avi Bryant <avi@stripe.com>
2019-08-29 21:11:52 -0700

    add test case

--- rainier-core/src/main/scala/com/stripe/rainier/optimizer/Optimizer.scala
+++ rainier-core/src/main/scala/com/stripe/rainier/optimizer/Optimizer.scala
@@ -7,11 +7,6 @@ object Optimizer {
     val x = new Array[Double](df.nVars)
     val g = new Array[Double](df.nVars)
 
-    //for LBFGSOrig
-    val x2 = new Array[Double](df.nVars)
-    val diag = new Array[Double](df.nVars)
-    val iflag = Array(0)
-
     var complete = false
     val m = 5
     val eps = 0.1
@@ -24,17 +19,6 @@ object Optimizer {
         i += 1
       }
       complete = lb(df.density * -1, g)
-
-      LBFGSOrig.lbfgs(x2.size,
-                      m,
-                      x2,
-                      df.density * -1,
-                      g,
-                      false,
-                      diag,
-                      eps,
-                      iflag)
-      assert(x.toList == x2.toList)
     }
     return x
   }
