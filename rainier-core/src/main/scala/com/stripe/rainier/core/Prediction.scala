04fdf0c784f57fccb16ffcfcfec89589316a771a
Avi Bryant <avi@avibryant.com>
2019-12-03 20:51:38 -0800

    delete prediction

--- rainier-core/src/main/scala/com/stripe/rainier/core/Prediction.scala
+++
@@ -1,9 +0,0 @@
-package com.stripe.rainier.core
-
-case class Prediction[T](model: Model, generator: Generator[T]) {
-  def map[U](fn: T => U): Prediction[U] = Prediction(model, generator.map(fn))
-  def flatMap[U, V](fn: T => Generator[U]): Prediction[U] =
-    Prediction(model, generator.flatMap(fn))
-  def zip[U](other: Prediction[U]): Prediction[(T, U)] =
-    Prediction(model.merge(other.model), generator.zip(other.generator))
-}
