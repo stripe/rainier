cfa0564942ca05f353376fa7fea8d5a81445c4ee
Avi Bryant <avi@avibryant.com>
2019-12-04 09:12:57 -0800

    sbctest

---
+++ rainier-tests/src/test/scala/com/stripe/rainier/core/SBCTest.scala
@@ -0,0 +1,29 @@
+package com.stripe.rainier.core
+
+import org.scalatest.FunSuite
+
+class SBCTest extends FunSuite {
+
+  def check[T](sbcModel: SBCModel[T]): Unit = {
+    test(sbcModel.description) {
+      assert(sbcModel.samples == sbcModel.goldset)
+    }
+  }
+
+  /** Continuous **/
+  check(SBCUniformNormal)
+  check(SBCLogNormal)
+  check(SBCExponential)
+  check(SBCLaplace)
+  check(SBCGamma)
+
+  /** Discrete **/
+  check(SBCBernoulli)
+  check(SBCBinomial)
+  //check(SBCBinomialNormalApproximation)
+  check(SBCBinomialPoissonApproximation)
+  check(SBCGeometric)
+  //check(SBCGeometricZeroInflated)
+  check(SBCNegativeBinomial)
+  check(SBCLargePoisson)
+}
