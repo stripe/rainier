d9689c05a56fe91257bfdf9a4846473e2b0f7ed7
Avi Bryant <avi@avibryant.com>
2019-12-05 09:57:04 -0800

    Restore benchmarks and SBC tests (#415)

---
+++ rainier-benchmark/src/main/scala/com/stripe/rainier/bench/SBCBenchmark.scala
@@ -0,0 +1,117 @@
+package com.stripe.rainier.bench
+
+import org.openjdk.jmh.annotations._
+import java.util.concurrent.TimeUnit
+
+import com.stripe.rainier.compute._
+import com.stripe.rainier.core._
+import com.stripe.rainier.ir.DataFunction
+import com.stripe.rainier.sampler._
+
+@BenchmarkMode(Array(Mode.SampleTime))
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 3, time = 1)
+@Measurement(iterations = 5, time = 1)
+@Fork(3)
+@Threads(4)
+@State(Scope.Benchmark)
+abstract class SBCBenchmark {
+  implicit val rng: RNG = RNG.default
+
+  protected def sbc: SBC[_]
+
+  @Param(Array("100", "1000", "10000", "100000"))
+  protected var syntheticSamples: Int = _
+
+  var s: SBC[_] = _
+  var model: Model = _
+  var df: DensityFunction = _
+
+  @Setup(Level.Trial)
+  def setup(): Unit = {
+    s = sbc
+    model = build
+    df = model.density
+    ()
+  }
+
+  @Benchmark
+  def synthesize(): (Seq[_], Double) =
+    s.synthesize(syntheticSamples)
+
+  @Benchmark
+  def build(): Model = s.model(syntheticSamples)._1
+
+  @Benchmark
+  def compile(): DataFunction =
+    Compiler.default.compileTargets(TargetGroup(model.targets, 0), true, 4)
+
+  @Benchmark
+  def run(): Unit =
+    df.update(Array.fill(df.nVars) { rng.standardUniform })
+}
+
+class NormalBenchmark extends SBCBenchmark {
+  def sbc =
+    SBC(Uniform(0, 1)) { n =>
+      Normal(n, 1)
+    }
+}
+
+class LaplaceBenchmark extends SBCBenchmark {
+  def sbc = SBC(LogNormal(0, 1)) { x =>
+    Laplace(x, x)
+  }
+}
+
+class LogNormalBenchmark extends SBCBenchmark {
+  def sbc =
+    SBC(LogNormal(0, 1))((x: Real) => LogNormal(x, x))
+}
+
+class ExponentialBenchmark extends SBCBenchmark {
+  def sbc =
+    SBC(LogNormal(0, 1))((x: Real) => Exponential(x))
+}
+
+class BernoulliBenchmark extends SBCBenchmark {
+  def sbc =
+    SBC(Uniform(0, 1))((x: Real) => Bernoulli(x))
+}
+
+class BinomialBenchmark extends SBCBenchmark {
+  def sbc =
+    SBC(Uniform(0, 1))((x: Real) => Binomial(x, 10))
+}
+
+class GeometricBenchmark extends SBCBenchmark {
+  def sbc =
+    SBC(Uniform(0, 1))((x: Real) => Geometric(x))
+}
+
+class GeometricZeroInflatedBenchmark extends SBCBenchmark {
+  def sbc =
+    SBC(Uniform(0, 1))((x: Real) => Geometric(.3).zeroInflated(x))
+}
+
+class NegativeBinomialBenchmark extends SBCBenchmark {
+  def sbc =
+    SBC(Uniform(0, 1))((x: Real) => NegativeBinomial(x, 10))
+}
+
+class BinomialPoissonApproximationBenchmark extends SBCBenchmark {
+  def sbc =
+    SBC(Uniform(0, 0.04))((x: Real) => Binomial(x, 200))
+}
+
+class GaussianMixtureBenchmark extends SBCBenchmark {
+  def sbc =
+    SBC(Uniform(0, 1))(
+      (x: Real) =>
+        Mixture(
+          Map(
+            Normal(0, 1) -> x,
+            Normal(1, 2) -> (Real.one - x)
+          )
+      ))
+}
---
+++ rainier-tests/src/test/scala/com/stripe/rainier/core/SBCTest.scala
@@ -0,0 +1,34 @@
+package com.stripe.rainier.core
+
+import org.scalatest.FunSuite
+
+class SBCTest extends FunSuite {
+
+  val Epsilon = 1e-10
+  def check[T](sbcModel: SBCModel[T]): Unit = {
+    test(sbcModel.description) {
+      sbcModel.samples.zip(sbcModel.goldset).foreach {
+        case (a, b) =>
+          val err = Math.abs((a - b) / b)
+          assert(err < Epsilon)
+      }
+    }
+  }
+
+  /** Continuous **/
+  check(SBCUniformNormal)
+  //check(SBCLogNormal)
+  check(SBCExponential)
+  check(SBCLaplace)
+  check(SBCGamma)
+
+  /** Discrete **/
+  check(SBCBernoulli)
+  check(SBCBinomial)
+  //check(SBCBinomialNormalApproximation)
+  check(SBCBinomialPoissonApproximation)
+  check(SBCGeometric)
+  //check(SBCGeometricZeroInflated)
+  check(SBCNegativeBinomial)
+  check(SBCLargePoisson)
+}
