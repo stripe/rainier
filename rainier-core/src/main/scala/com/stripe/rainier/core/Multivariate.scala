c16cc58730e48dea4d87fc318470b4677db291d4
Avi Bryant <avi@avibryant.com>
2020-02-04 18:33:09 -0800

    wip

--- rainier-core/src/main/scala/com/stripe/rainier/core/Multivariate.scala
+++ rainier-core/src/main/scala/com/stripe/rainier/core/Multivariate.scala
@@ -5,6 +5,6 @@ import com.stripe.rainier.compute._
 case class MVNormal(k: Int, chol: Cholesky) extends Distribution[Seq[Double]] {
  def logDensity(seq: Seq[Seq[Double]]) = ???   
  def generator = Generator(chol.packed).zip(Generator(Normal.standard).repeat(k)).map{
-     case (a, z) => Cholesky.multiply(a.toArray, z.toArray)
+     case (a, z) => Cholesky.lowerTriangularMultiply(a.toArray, z.toArray)
  }
 }
