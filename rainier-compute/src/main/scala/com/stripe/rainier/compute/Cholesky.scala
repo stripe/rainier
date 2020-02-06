84fa803ca74b126de3de69653939993ca8079042
Avi Bryant <avi@avibryant.com>
2020-02-06 09:43:17 -0800

    pass tests for upper triangular solve

--- rainier-compute/src/main/scala/com/stripe/rainier/compute/Cholesky.scala
+++ rainier-compute/src/main/scala/com/stripe/rainier/compute/Cholesky.scala
@@ -5,82 +5,87 @@ This represents an NxN matrix A
 by the  N(N+1)/2 packed lower-triangular elements
 of its Cholesky decomposition L
 (where A = LL*)
-*/
+ */
 case class Cholesky(packed: Vector[Real]) {
-    val rank: Int = Cholesky.rank(packed.size)
-    
-    //log(det(A))
-    def logDeterminant: Real = Real.sum(diagonals.map(_.log)) * 2
+  val rank: Int = Cholesky.rank(packed.size)
 
-    //solve for x where Ax=y
-    def inverseMultiply(y: Vector[Real]): Vector[Real] = {
-        //L*Lx = y, let b=Lx 
-        //solve L*b = y
-        val b = Cholesky.upperTriangularSolve(packed, y)
-        //solve Lx = b
-        Cholesky.lowerTriangularSolve(packed, b)
+  //log(det(A))
+  def logDeterminant: Real = Real.sum(diagonals.map(_.log)) * 2
+
+  //solve for x where Ax=y
+  def inverseMultiply(y: Vector[Real]): Vector[Real] = {
+    //L*Lx = y, let b=Lx
+    //solve L*b = y
+    val b = Cholesky.upperTriangularSolve(packed, y)
+    //solve Lx = b
+    Cholesky.lowerTriangularSolve(packed, b)
+  }
+
+  //diagonal elements of L
+  private def diagonals: Vector[Real] =
+    0.until(rank).toVector.map { i =>
+      packed(Cholesky.triangleNumber(i))
     }
-
-    //diagonal elements of L
-    private def diagonals: Vector[Real] = 
-        0.until(rank).toVector.map{i =>
-            packed(Cholesky.triangleNumber(i))
-        }
 }
 
 object Cholesky {
-    def lowerTriangularMultiply(packed: Array[Double], vector: Array[Double]): Array[Double] = {
-        val n = vector.size
-        val result = new Array[Double](n)
-        var i = 0
-        var base = 0
-        while(i < n) {
-            var j = 0
-            var acc = 0d
-            while(j <= i) {
-                acc += vector(j) * packed(base + j)
-                j += 1
-            }
-            result(i) = acc
-            i += 1
-            base += i                
-        }
-        result
+  def lowerTriangularMultiply(packed: Array[Double],
+                              vector: Array[Double]): Array[Double] = {
+    val n = vector.size
+    val result = new Array[Double](n)
+    var i = 0
+    var base = 0
+    while (i < n) {
+      var j = 0
+      var acc = 0d
+      while (j <= i) {
+        acc += vector(j) * packed(base + j)
+        j += 1
+      }
+      result(i) = acc
+      i += 1
+      base += i
     }
-    
-    def triangleNumber(k: Int) = (k * (k+1)) / 2
-    def rank(packedSize: Int) = Math.sqrt(packedSize * 2.0).floor.toInt
+    result
+  }
 
-    //solve Lx = y
-    def lowerTriangularSolve(packed: Vector[Real], y: Vector[Real]): Vector[Real] =
-        //forward substitution
-        y.toList.zipWithIndex.foldLeft(Vector.empty[Real]){
-            case (xAcc, (yi, i)) => 
-                val row = packed.slice(triangleNumber(i), triangleNumber(i+1))
-                val dot = Real.sum(row.init.zip(xAcc).map{case (a,b) => a*b})
-                val xi = (yi - dot) / row.last
-                xAcc :+ xi
-        }
+  //number of non-zero elements in the first k rows of a triangular matrix
+  def triangleNumber(k: Int): Int = (k * (k + 1)) / 2
 
-    //solve L*x = y
-    def upperTriangularSolve(packed: Vector[Real], y: Vector[Real]): Vector[Real] =
-        //let z = y.reverse, and let w = x.reverse
-        //"rotate" L to be a lower-triangular matrix R,
-        //such that Rw = z implies that  L*x = y
-        lowerTriangularSolve(rotate(packed), y.reverse).reverse
+  private def packedIndex(i: Int, j: Int): Int =
+    triangleNumber(i) + j
 
-    private def rotateIndex(i: Int, j: Int, n: Int): Int =
-        triangleNumber(n-i) +
-        triangleNumber(n-i+j-1) -
-        triangleNumber(n-i-1) -
-        1
+  def rank(packedSize: Int) = Math.sqrt(packedSize * 2.0).floor.toInt
 
-    private def rotate(packed: Vector[Real]): Vector[Real] = {
-        val n = rank(packed.size)
-        0.until(n).flatMap{i => 
-            0.to(i).map{j => 
-                packed(rotateIndex(i, j, n))
-            }
-        }.toVector
+  //solve Lx = y
+  def lowerTriangularSolve(packed: Vector[Real],
+                           y: Vector[Real]): Vector[Real] =
+    //forward substitution
+    y.toList.zipWithIndex.foldLeft(Vector.empty[Real]) {
+      case (xAcc, (yi, i)) =>
+        val row = packed.slice(triangleNumber(i), triangleNumber(i + 1))
+        val dot = Real.sum(row.init.zip(xAcc).map { case (a, b) => a * b })
+        val xi = (yi - dot) / row.last
+        xAcc :+ xi
     }
+
+  //solve L*x = y
+  def upperTriangularSolve(packed: Vector[Real],
+                           y: Vector[Real]): Vector[Real] =
+    //let z = y.reverse, and let w = x.reverse
+    //"reflect" L to be a lower-triangular matrix R,
+    //such that Rw = z implies that  L*x = y
+    lowerTriangularSolve(reflect(packed), y.reverse).reverse
+
+  //the reflection of m[i,j] is m[n-j-1,n-i-1]
+  private def reflect(packed: Vector[Real]): Vector[Real] = {
+    val n = rank(packed.size)
+    0.until(n)
+      .flatMap { i =>
+        0.to(i).map { j =>
+          packed(packedIndex(n - j - 1, n - i - 1))
+        }
+      }
+      .toVector
+  }
 }
--- rainier-core/src/main/scala/com/stripe/rainier/core/Multivariate.scala
+++ rainier-core/src/main/scala/com/stripe/rainier/core/Multivariate.scala
@@ -4,64 +4,66 @@ import com.stripe.rainier.compute._
 
 //multivariate continuous
 trait Multivariate extends Distribution[Seq[Double]] {
-    def k: Int
-    def rv: Vec[Real]
+  def k: Int
+  def rv: Vec[Real]
 
-    def logDensity(seq: Seq[Seq[Double]]) =
-        Vec.from(seq).map(logDensity).columnize
+  def logDensity(seq: Seq[Seq[Double]]) =
+    Vec.from(seq).map(logDensity).columnize
 
-    def logDensity(x: Vec[Real]): Real
+  def logDensity(x: Vec[Real]): Real
 }
 
-case class MVNormal(k: Int, locations: Vec[Real], cov: Covariance) extends Multivariate {
- def rv: Vec[Real] = ???
+case class MVNormal(k: Int, locations: Vec[Real], cov: Covariance)
+    extends Multivariate {
+  def rv: Vec[Real] = ???
 
-  def logDensity(x: Vec[Real]) = 
-    ((Real.Pi * 2).log + 
-    cov.logDeterminant + 
-    x.dot(cov.inverseMultiply(x))) / -2
+  def logDensity(x: Vec[Real]) =
+    ((Real.Pi * 2).log +
+      cov.logDeterminant +
+      x.dot(cov.inverseMultiply(x))) / -2
 
   def generator = {
     val iidNormals = Normal.standard.generator.repeat(k).map(_.toArray)
-    cov.choleskyGenerator.zip(iidNormals).map{
-     case (a, z) => Cholesky.lowerTriangularMultiply(a, z)
+    cov.choleskyGenerator.zip(iidNormals).map {
+      case (a, z) => Cholesky.lowerTriangularMultiply(a, z)
     }
-  }    
+  }
 }
-
+/*
 object MVNormal {
-    def eta(k: Int, eta: Real): MVNormal = 
-        MVNormal(k, LKJCholesky(eta, Vec.from(List.fill(k)(1))))
-    def eta(k: Int, eta: Real, sigma: Continuous): MVNormal = 
-        MVNormal(k, LKJCholesky(eta, sigma.vec(k)))
-    def rho(k: Int, rho: Map[(Int,Int),Real]): MVNormal =
-        MVNormal(k, RhoSigma(rho, Vec.from(List.fill(k)(1))))
-    def rho(k: Int, rho: Map[(Int,Int),Real], sigma: Continuous): MVNormal =
-        MVNormal(k, RhoSigma(rho, sigma.vec(k)))
+  def eta(k: Int, eta: Real): MVNormal =
+    MVNormal(k, LKJCholesky(eta, Vec.from(List.fill(k)(1))))
+  def eta(k: Int, eta: Real, sigma: Continuous): MVNormal =
+    MVNormal(k, LKJCholesky(eta, sigma.vec(k)))
+  def rho(k: Int, rho: Map[(Int, Int), Real]): MVNormal =
+    MVNormal(k, RhoSigma(rho, Vec.from(List.fill(k)(1))))
+  def rho(k: Int, rho: Map[(Int, Int), Real], sigma: Continuous): MVNormal =
+    MVNormal(k, RhoSigma(rho, sigma.vec(k)))
 }
-
+ */
 trait Covariance {
-    //generates in packed lower-triangular representation
-    def choleskyGenerator: Generator[Array[Double]]
+  //generates in packed lower-triangular representation
+  def choleskyGenerator: Generator[Array[Double]]
 
-    def logDeterminant: Real
-    def inverseMultiply(x: Vec[Real]): Vec[Real]
+  def logDeterminant: Real
+  def inverseMultiply(x: Vec[Real]): Vec[Real]
 }
 
-case class RhoSigma(rho: Map[(Int,Int),Real], sigmas: Vec[Real]) extends Covariance {
-    def choleskyGenerator = ???
+case class RhoSigma(rho: Map[(Int, Int), Real], sigmas: Vec[Real])
+    extends Covariance {
+  def choleskyGenerator = ???
 
-    def logDeterminant = ???
-    def inverseMultiply(x: Vec[Real]) = ???
+  def logDeterminant = ???
+  def inverseMultiply(x: Vec[Real]) = ???
 }
 
 case class LKJCholesky(eta: Real, sigmas: Vec[Real]) extends Covariance {
-    val cholesky: Cholesky = ???
+  val cholesky: Cholesky = ???
 
-    val choleskyGenerator =
-        Generator(cholesky.packed).map(_.toArray)
+  val choleskyGenerator =
+    Generator(cholesky.packed).map(_.toArray)
 
-    val logDeterminant = cholesky.logDeterminant
-    def inverseMultiply(x: Vec[Real]): Vec[Real] =
-        Vec.from(cholesky.inverseMultiply(x.toVector))
+  val logDeterminant = cholesky.logDeterminant
+  def inverseMultiply(x: Vec[Real]): Vec[Real] =
+    Vec.from(cholesky.inverseMultiply(x.toVector))
 }
