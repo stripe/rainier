06b605c6b159427e57b1809b299fbd0ebe97dc2a
Avi Bryant <avi@avibryant.com>
2020-02-12 19:24:51 -0800

    wip

--- rainier-sampler/src/main/scala/com/stripe/rainier/sampler/Progress.scala
+++ rainier-sampler/src/main/scala/com/stripe/rainier/sampler/Progress.scala
@@ -28,6 +28,7 @@ case class ProgressState(outputEverySeconds: Double,
 
   def start(): Unit = {
     startTime = System.nanoTime()
+    lastOutputTime = System.nanoTime()
   }
 
   def finish(): Unit = {
