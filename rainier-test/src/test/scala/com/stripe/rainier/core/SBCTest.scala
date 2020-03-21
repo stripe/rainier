548c12bc759748ec7cf9b6fa7a0c2a1feabf6948
Avi Bryant <avi@avibryant.com>
2020-03-21 12:27:31 -0700

    temporarily remove multivariate

--- rainier-test/src/test/scala/com/stripe/rainier/core/SBCTest.scala
+++ rainier-test/src/test/scala/com/stripe/rainier/core/SBCTest.scala
@@ -22,7 +22,6 @@ class SBCTest extends FunSuite {
   check(SBCExponential)
   check(SBCLaplace)
   check(SBCGamma) //Couldn't prove x >= 0 for bounds (-Infinity,Infinity)
-  check(SBCBVNormal)
 
   // Discrete
   check(SBCBernoulli)
